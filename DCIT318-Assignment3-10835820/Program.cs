f8d745776f4d76a33d88cae5301b8763ce7d80da
siah Kofi Dennis <nsiahkofidennis001@.com>
2025-08-11 15:24:06 -0700

    eigth commit

--- DCIT318-Assignment3-10835820/Program.cs
+++ DCIT318-Assignment3-10835820/Program.cs
@@ -143,5 +143,23 @@ namespace DCIT318_Assignment3
         }
     }
 
-      // Patient
+    // Patient
+    public class Patient
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Age { get; }
+        public string Gender { get; }
+
+        public Patient(int id, string name, int age, string gender)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public override string ToString() => $"Patient {Name} (ID: {Id}, Age: {Age}, Gender: {Gender})";
+    }
+
 
