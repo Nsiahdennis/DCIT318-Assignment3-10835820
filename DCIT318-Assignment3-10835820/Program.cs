928727d78d2d87859fbb350c76481044d372d02d
siah Kofi Dennis <nsiahkofidennis001@.com>
2025-08-11 15:19:53 -0700

    forth commit

--- DCIT318-Assignment3-10835820/Program.cs
+++ DCIT318-Assignment3-10835820/Program.cs
@@ -83,3 +83,41 @@ namespace DCIT318_Assignment3
             }
         }
     }
+    // f) FinanceApp
+    public class FinanceApp
+    {
+        private readonly List<Transaction> _transactions = new();
+
+        public void Run()
+        {
+            Console.WriteLine("=== FinanceApp Demo ===");
+
+            // i. Instantiate SavingsAccount
+            var account = new SavingsAccount("SA-1001", 1000m);
+
+            // ii. Create 3 transactions
+            var t1 = new Transaction(1, DateTime.Now, 150.00m, "Groceries");
+            var t2 = new Transaction(2, DateTime.Now, 250.00m, "Utilities");
+            var t3 = new Transaction(3, DateTime.Now, 900.00m, "Entertainment"); // large to test insufficient funds
+
+            // iii. Use processors
+            ITransactionProcessor p1 = new MobileMoneyProcessor();
+            ITransactionProcessor p2 = new BankTransferProcessor();
+            ITransactionProcessor p3 = new CryptoWalletProcessor();
+
+            p1.Process(t1);
+            p2.Process(t2);
+            p3.Process(t3);
+
+            // iv. Apply to account
+            account.ApplyTransaction(t1);
+            account.ApplyTransaction(t2);
+            account.ApplyTransaction(t3); // Should show insufficient funds if amount > balance
+
+            // v. Add to _transactions
+            _transactions.AddRange(new[] { t1, t2, t3 });
+
+            Console.WriteLine("FinanceApp finished.\n");
+        }
+    }
+
